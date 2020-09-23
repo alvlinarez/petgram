bf92f6ffca00e25463cffa82ad3b3dfd360d1d38
Alvaro Linarez <alvarolinarez9@gmail.com>
2020-09-22 19:05:47 -0500

    Pet page added

--- pages/pet/[id].js
+++ pages/pet/[id].js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Layout from '../../components/layout/Layout';
+import { useRouter } from 'next/router';
+import { AuthContext } from '../../context/auth/AuthContext';
+import { useQuery } from '@apollo/client';
+import Error from '../../components/Error';
+import { GET_PHOTO } from '../../apollo/queries/getPhoto';
+import { PhotoLoader } from '../../components/loaders';
+import PhotoCard from '../../components/photo/PhotoCard';
 
 const Pet = () => {
-  return <div></div>;
+  const router = useRouter();
+  const {
+    query: { id }
+  } = router;
+
+  // Get user info
+  const authContext = useContext(AuthContext);
+  const { authenticated, user } = authContext;
+
+  // Query to get photo
+  const { data, loading, error } = useQuery(GET_PHOTO, {
+    variables: {
+      id
+    }
+  });
+
+  let photo = {};
+  if (!loading && data) {
+    photo = data.getPhoto;
+    if (authenticated) {
+      if (authenticated) {
+        photo = {
+          ...photo,
+          liked: !!photo.usersLiked.find(
+            (userLiked) => userLiked.id === user.id
+          )
+        };
+      }
+    }
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <Layout title={`Photo ${id}`}>
+      {loading ? <PhotoLoader /> : <PhotoCard {...photo} />}
+    </Layout>
+  );
 };
 
 export default Pet;
